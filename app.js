754f9db10923c7d40bff92101c46094ef5e8a547
Siddhartha Sarkar <siddhartha.bd@gmail.com>
2022-01-17 00:58:59 +0900

    redirect

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function (request, response) {
     if (request.headers.host.includes('new.')) {
         request.url = '/hello';
         return app._router.handle(request, response);
+
     } else if (request.headers.host.includes('sarker')) {
         request.url = '/hi';
         return app._router.handle(request, response);
@@ -14,6 +15,11 @@ app.get('/', function (request, response) {
         response.send({"msg": `${request.headers.host}}: this is orgin`});
     }
 });
+
+// app.get('*', (req, res) => {
+//     // REDIRECT goes here
+//     res.redirect('https://www.YOUR_URL.com/')
+// });
 
 app.get('/hello', function (request, response) {
     response.send({"msg": `${request.headers.host}: this is path of HELLO`});
